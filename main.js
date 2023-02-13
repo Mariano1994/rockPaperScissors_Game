a953c3300aa6bcbdcd880926e769a13e6af0c600
Mariano <marianocapiliku@gmail.com>
2023-02-14 00:47:22 +0100

    function to play the game

--- main.js
+++ main.js
@@ -1,31 +1,7 @@
-// const userName = prompt("inform your name");
-// alert(`Welcome to Rock Paper Scissor Game ${userName}.
-// Let's play!!`);
-
-// const computerChose =
-//   items[Math.floor(Math.random() * items.length)].toLowerCase();
-
-// if (userChose === "rock" && computerChose === "csissor") {
-//   console.log(`Congratulations! ${userName} you Won!`);
-// } else if (userChose === "paper" && computerChose === "rock") {
-//   console.log(`Congratulations! ${userName} you Won!`);
-// } else if (userChose === "csissor" && computerChose === "paper") {
-//   console.log(`Congratulations! ${userName} you Won!`);
-// } else if (computerChose === "rock" && userChose === "csissor") {
-//   console.log(`Sorry ${userName} you lost`);
-// } else if (computerChose === "paper" && userChose === "rock") {
-//   console.log(`Sorry ${userName} you lost`);
-// } else if (computerChose === "csissor" && userChose === "paper") {
-//   console.log(`Sorry ${userName} you lost`);
-// } else {
-//   console.log("Tied");
-// }
-
 // FUNCTION TO GET THE USER CHOSE
-const getUserChose = function () {
+const getUserChoice = function () {
   return prompt("Infomr you chose: ROCK PAPER or CSISSOR").toLocaleLowerCase();
 };
-console.log(getUserChose());
 
 // FUNCTION TO GET THE COMPUTER CHOSE
 const items = ["Paper", "Rock", "Scissor"];
@@ -33,4 +9,26 @@ const getComputerSelection = function () {
   return items[Math.floor(Math.random() * items.length)].toLocaleLowerCase();
 };
 
-console.log(getComputerSelection());
+// FUNCTION TO PLAY HE GAME
+const playRound = function (
+  computer = getComputerSelection(),
+  user = getUserChoice()
+) {
+  if (user === "rock" && computer === "csissor") {
+    return `You Won! ${user} beats ${computer}`;
+  } else if (user === "paper" && computer === "rock") {
+    return `You Won! ${user} beats ${computer}`;
+  } else if (user === "csissor" && computer === "paper") {
+    return `You Won! ${user} beats ${computer}`;
+  } else if (computer === "rock" && user === "csissor") {
+    return `You Lost! ${computer} beats ${user}`;
+  } else if (computer === "paper" && user === "rock") {
+    return `You Lost! ${computer} beats ${user}`;
+  } else if (computer === "csissor" && user === "paper") {
+    return `You Lost! ${computer} beats ${user}`;
+  } else {
+    return "Tied";
+  }
+};
+
+console.log(playRound());
