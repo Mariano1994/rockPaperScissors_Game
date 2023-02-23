8e876b7ceabe9fe460d81804dad542b370d75b12
Mariano <marianocapiliku@gmail.com>
2023-02-24 00:15:06 +0100

    creating function to play the game Use slice string method to get the right selection from user Using if statment to compare userChoice with the computer selection

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-// FUNCTION TO GET THE USER CHOICE
-const getUserChoice = function () {
-  return prompt("Infomr you chose: ROCK PAPER or CSISSOR").toLocaleLowerCase();
-};
+// SELECTING RESULT ELEMENT
+const result = document.querySelector(".result");
 
 // FUNCTION TO GET THE COMPUTER CHOICE
 const items = ["Paper", "Rock", "Csissor"];
@@ -10,29 +8,65 @@ const getComputerSelection = function () {
 };
 
 // FUNCTION TO PLAY THE GAME
-const playRound = function (
-  computer = getComputerSelection(),
-  user = getUserChoice()
-) {
-  if (user === "rock" && computer === "csissor") {
-    return `You Won! ${user} beats ${computer}`;
-  } else if (user === "paper" && computer === "rock") {
-    return `You Won! ${user} beats ${computer}`;
-  } else if (user === "csissor" && computer === "paper") {
-    return `You Won! ${user} beats ${computer}`;
-  } else if (computer === "rock" && user === "csissor") {
-    return `You Lost! ${computer} beats ${user}`;
-  } else if (computer === "paper" && user === "rock") {
-    return `You Lost! ${computer} beats ${user}`;
-  } else if (computer === "csissor" && user === "paper") {
-    return `You Lost! ${computer} beats ${user}`;
-  } else {
-    return "Tied";
-  }
-};
+function playRound() {
+  const userSelection = document.querySelectorAll("a");
+  userSelection.forEach((item) => {
+    item.addEventListener("click", () => {
+      const userChoice = item.textContent.slice(4);
 
-// FUNCTION TO PLAY THE GAME FIVE TIMES
-const game = function () {
-  console.log(playRound());
-};
-game();
+      // CHECKING SELECTION
+      if (userChoice === "rock" && getComputerSelection() === "csissor") {
+        result.textContent = `You won ${userChoice.toUpperCase()} beats CSISSOR!`;
+      } else if (userChoice === "paper" && getComputerSelection() === "rock") {
+        result.textContent = `You won ${userChoice.toUpperCase()} beats ROCK!`;
+      } else if (
+        userChoice === "csissor" &&
+        getComputerSelection() === "paper"
+      ) {
+        result.textContent = `You won ${userChoice.toUpperCase()} beats PAPER!`;
+      } else if (
+        getComputerSelection() === "rock" &&
+        userChoice === "csissor"
+      ) {
+        result.textContent = `You lose ROCK beats ${userChoice}`;
+      } else if (getComputerSelection() === "paper" && userChoice === "rock") {
+        result.textContent = `You lose PAPER beats ${userChoice}`;
+      } else if (
+        getComputerSelection() === "csissor" &&
+        userChoice === "paper"
+      ) {
+        result.textContent = `You lose CSISSOR beats ${userChoice}`;
+      } else {
+        result.textContent = ` Tied`;
+      }
+    });
+  });
+}
+
+// // FUNCTION TO PLAY THE GAME
+// const playRound = function (
+//   computer = getComputerSelection(),
+//   user = getUserChoice()
+// ) {
+//   if (user === "rock" && computer === "csissor") {
+//     return `You Won! ${user} beats ${computer}`;
+//   } else if (user === "paper" && computer === "rock") {
+//     return `You Won! ${user} beats ${computer}`;
+//   } else if (user === "csissor" && computer === "paper") {
+//     return `You Won! ${user} beats ${computer}`;
+//   } else if (computer === "rock" && user === "csissor") {
+//     return `You Lost! ${computer} beats ${user}`;
+//   } else if (computer === "paper" && user === "rock") {
+//     return `You Lost! ${computer} beats ${user}`;
+//   } else if (computer === "csissor" && user === "paper") {
+//     return `You Lost! ${computer} beats ${user}`;
+//   } else {
+//     return "Tied";
+//   }
+// };
+
+// // FUNCTION TO PLAY THE GAME FIVE TIMES
+// const game = function () {
+//   console.log(playRound());
+// };
+// game();
