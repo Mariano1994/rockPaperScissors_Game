422e029d52e2721e54ed8190e5a196fca2f3e0b5
Mariano <marianocapiliku@gmail.com>
2023-02-24 02:11:49 +0100

    Add functionality to check score add fubcitonality to restart the buttom to play again

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-// SELECTING RESULT ELEMENT
+// SELECTING ELEMENT
 const result = document.querySelector(".result");
+const userScore = document.querySelector(".user");
+const computerScore = document.querySelector(".comp");
+const restartButton = document.querySelector(".button");
 let userConter = 0;
 let computerConter = 0;
 
@@ -19,42 +22,60 @@ function playRound() {
       // CHECKING SELECTION
       if (userChoice === "rock" && getComputerSelection() === "csissor") {
         userConter++;
+        userScore.textContent = `User: ${userConter}`;
         result.textContent = `${userChoice.toUpperCase()} beats CSISSOR!`;
       } else if (userChoice === "paper" && getComputerSelection() === "rock") {
         userConter++;
+        userScore.textContent = `User: ${userConter}`;
         result.textContent = `${userChoice.toUpperCase()} beats ROCK!`;
       } else if (
         userChoice === "csissor" &&
         getComputerSelection() === "paper"
       ) {
         userConter++;
+        userScore.textContent = `User: ${userConter}`;
         result.textContent = `${userChoice.toUpperCase()} beats PAPER!`;
       } else if (
         getComputerSelection() === "rock" &&
         userChoice === "csissor"
       ) {
         computerConter++;
+        computerScore.textContent = `Computer: ${computerConter}`;
         result.textContent = ` ROCK beats ${userChoice.toUpperCase()}`;
       } else if (getComputerSelection() === "paper" && userChoice === "rock") {
         computerConter++;
+        computerScore.textContent = `Computer: ${computerConter}`;
         result.textContent = ` PAPER beats ${userChoice.toUpperCase()}`;
       } else if (
         getComputerSelection() === "csissor" &&
         userChoice === "paper"
       ) {
         computerConter++;
+        computerScore.textContent = `Computer: ${computerConter}`;
         result.textContent = `CSISSOR beats ${userChoice.toUpperCase()}`;
       } else {
         result.textContent = ` Tied`;
       }
 
       if (userConter == 5) {
-        result.textContent = `You Woonn the Game!!`;
+        result.textContent = `You Won the Game!!`;
       } else if (computerConter === 5) {
         result.textContent = `You lose the game`;
       }
     });
   });
 }
+
+// RESTART GAME
+
+restartButton.addEventListener("click", () => {
+  computerConter = 0;
+  userConter = 0;
+
+  userScore.textContent = `User: 0`;
+  computerScore.textContent = `Computer: 0`;
+
+  result.textContent = `Result`;
+});
 
 playRound();
